7007e76de729a4b6db9ea7f87fa5240478223bfa
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-04-07 00:13:18 +0400

    DostaZengProject

---
+++ Project1/Project1/Models/Hospital.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    class Hospital
+    {
+        private  Appointment[] appointments = new Appointment[18];
+        private int appointmentCount = 0;
+
+        public void  AddAppointment (Appointment appointment )
+        {
+            if(appointmentCount < appointments.Length)
+            {
+                appointments[appointmentCount] = appointment;
+                appointmentCount++;
+                Console.WriteLine("Gorus teyin edildi");
+            }
+            else
+            {
+                Console.WriteLine("Bos vaxt yoxdur");
+            }
+        }
+
+        public void EndAppointment(int no)
+        {
+            if(no > 0 && no < appointmentCount)
+            {
+                appointments[no].EndDate = DateTime.Now;
+                Console.WriteLine($"Xeste {appointments[no].Patient} ucun gorus bitti:{appointments[no].EndDate}" );
+            }
+            else
+            {
+                Console.WriteLine("Sehv deyer");
+            }
+        }
+        
+        public Appointment GetAppointment(int no)
+        {
+            if(no > 0 && no < appointmentCount)
+            {
+                return appointments[no];
+            }
+            return null;
+        }
+
+
+
+        public void GetAllAppointments()
+        {
+            Console.WriteLine("Butun gorusler:");
+            for (int no=0;no<appointmentCount;no++)
+            {
+                Console.WriteLine($"Xeste:{appointments[no].Patient} ; Hekim:{appointments[no].Doctor} ; Gorus basladi:{appointments[no].StartDate} ; Gorus bitti:{appointments[no].EndDate}");
+            }
+        }
+
+        public void GetAllContinuingAppointments()
+        {
+            Console.WriteLine("Bitmemis gorusler");
+            for (int no = 0; no < appointmentCount; no++)
+            {
+                if (appointments[no].EndDate == null)
+                {
+                    Console.WriteLine($"Xeste:{appointments[no].Patient} ; Hekim:{appointments[no].Doctor} ; Gorus basladi:{appointments[no].StartDate} ; Gorus bitti: Bitmemis");
+                }
+            }
+        }
+    }
+}
---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,56 @@
+﻿using Project1.Models;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospital hospital = new Hospital();
+
+            while(true)
+            {
+                Console.WriteLine("1.Yeni gorus yarat");
+                Console.WriteLine("2.Gorusu bitir");
+                Console.WriteLine("3.Butun goruslere bax");
+                Console.WriteLine("4.Bitmemis goruslere bax");
+                Console.WriteLine("5.Cix");
+                Console.Write("Seciminizi qeyd edin:");
+                string info = Console.ReadLine();
+
+                switch (info)
+                {
+                    case "1":
+
+                        Console.Write("Xestenin adi:");
+                        string patient = Console.ReadLine();
+
+                        Console.Write("Hekimin adi:");
+                        string doctor = Console.ReadLine();
+
+                        Console.Write("Baslama saati:");
+                        DateTime startDate = DateTime.Parse (Console.ReadLine());                     
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Bitirmek istediyiniz gorusu qeyd edin:");
+                        int endAppointmentNo = int.Parse(Console.ReadLine());
+
+                        break;
+
+                    case "3":
+                        hospital.GetAllAppointments();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Exit");
+                        break;
+
+                    default:
+                        Console.WriteLine("Yanlis secim.Bir daha sinayin");
+                        break;
+                }
+            }
+        }
+    }
+}
